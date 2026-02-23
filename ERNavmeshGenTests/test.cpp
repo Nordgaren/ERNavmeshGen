fd07e96b390a361b3b3d5d3d7bc74892572ed0ce
nordgaren <nordgarentv@gmail.com>
2026-02-23 02:09:52 -0700

    naming

--- ERNavmeshGenTests/test.cpp
+++ ERNavmeshGenTests/test.cpp
@@ -2,11 +2,11 @@
 #include "../ERNavmeshGen/API.h"
 
 // Your eldenring.exe path
-const char* er = std::getenv("ERNAVMA_GAME_PATH");
+const char* eldenRingGamePath = std::getenv("ERNAVMA_GAME_PATH");
 
 TEST(ERNAVMA_BATCH_CONVERT, EXPORTS)
 {
-  SetGamePath(er);
+  SetGamePath(eldenRingGamePath);
   const char* in = std::getenv("ERNAVMA_BATCH_CONVERT_IN"); // ERNAVMA_BATCH_CONVERT_IN = P:\ath\to\test\folder\;
   // Leave this empty if you are testing on collisions made with modding tools.
   const char* compendium = std::getenv("ERNAVMA_BATCH_CONVERT_COMPENDIUM"); // ERNAVMA_BATCH_CONVERT_COMPENDIUM = P:\ath\to\test.compendium;
@@ -16,7 +16,7 @@ TEST(ERNAVMA_BATCH_CONVERT, EXPORTS)
 
 TEST(ERNAVMA_CONVERT, EXPORTS)
 {
-  SetGamePath(er);
+  SetGamePath(eldenRingGamePath);
   const char* in = std::getenv("ERNAVMA_CONVERT_IN"); // ERNAVMA_CONVERT_IN = P:\ath\to\test.hkx;
   // Leave this empty if you are testing on collisions made with modding tools.
   const char* compendium = std::getenv("ERNAVMA_CONVERT_COMPENDIUM"); // ERNAVMA_CONVERT_COMPENDIUM = P:\ath\to\test.compendium;
