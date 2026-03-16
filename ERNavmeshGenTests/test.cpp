d627e676d6fd75bbdd67ee4fb8294557bfb14fe8
nordgaren <nordgarentv@gmail.com>
2026-03-16 16:13:58 -0700

    final test.cpp derp

--- ERNavmeshGenTests/test.cpp
+++ ERNavmeshGenTests/test.cpp
@@ -4,15 +4,15 @@
 // Your eldenring.exe path
 const char* eldenRingGamePath = std::getenv("ERNAVMA_GAME_PATH");
 
-// TEST(ERNAVMA_BATCH_CONVERT, EXPORTS)
-// {
-//     SetGamePath(eldenRingGamePath);
-//     const char* in = std::getenv("ERNAVMA_BATCH_CONVERT_IN"); // ERNAVMA_BATCH_CONVERT_IN = P:\ath\to\test\folder\;
-//     const char* out = std::getenv("ERNAVMA_BATCH_CONVERT_OUT"); // ERNAVMA_BATCH_CONVERT_OUT = P:\ath\to\out\folder\;
-//     // Leave this empty if you are testing on collisions made with modding tools.
-//     const char* compendium = std::getenv("ERNAVMA_BATCH_CONVERT_COMPENDIUM"); // ERNAVMA_BATCH_CONVERT_COMPENDIUM = P:\ath\to\test.compendium;
-//     EXPECT_TRUE(BatchGenerateNavMeshFromCollisionAPI(R"(C:\Users\rscos\Documents\mod-dev\elden-scrolls\mod\cache\terrain\)", R"(C:\Users\rscos\Documents\mod-dev\elden-scrolls\mod\cache\terrain\)", compendium));
-// }
+TEST(ERNAVMA_BATCH_CONVERT, EXPORTS)
+{
+    SetGamePath(eldenRingGamePath);
+    const char* in = std::getenv("ERNAVMA_BATCH_CONVERT_IN"); // ERNAVMA_BATCH_CONVERT_IN = P:\ath\to\test\folder\;
+    const char* out = std::getenv("ERNAVMA_BATCH_CONVERT_OUT"); // ERNAVMA_BATCH_CONVERT_OUT = P:\ath\to\out\folder\;
+    // Leave this empty if you are testing on collisions made with modding tools.
+    const char* compendium = std::getenv("ERNAVMA_BATCH_CONVERT_COMPENDIUM"); // ERNAVMA_BATCH_CONVERT_COMPENDIUM = P:\ath\to\test.compendium;
+    EXPECT_TRUE(BatchGenerateNavMeshFromCollisionAPI(in, out, compendium));
+}
 
 TEST(ERNAVMA_CONVERT, EXPORTS)
 {
